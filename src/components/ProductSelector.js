f94ec83789bb130858f9574cf363683f8d48a24f
Sheldon Lynn <sheldonlynn@outlook.com>
2018-05-16 00:38:52 -0700

    header fixes

--- src/components/ProductSelector.js
+++ src/components/ProductSelector.js
@@ -41,7 +41,7 @@ class ProductSelector extends Component {
     return (
       <div className="Modal-background">
         <div className="Modal-content">
-          <h1>Choose A Product</h1>
+          <h1>Pick A Book</h1>
           <div className="product-selector-radio">
             <RadioButtonGroup name="productSelection" onChange={this.handleChange}>
               {buttons}
